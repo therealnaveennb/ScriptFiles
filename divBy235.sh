01921f0815cd4bcd955dfe29cb78ae2a9eb8d446
therealnaveennb <s.naveenbharathi2603@gmail.com>
2024-10-07 08:47:03 +0000

    Added divisible by 3 and 5 file

---
+++ divBy235.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+##################
+#AUTHOR : NAVEEN BHARATHI S 
+
+#DATE :07/10/2024
+
+#TITLE : WRITE A PROGRAM TO FIND THE NUMBER WHICH CAN BE DIVISIBLE BY 5 OR 3 BUT NOT BY BOTH
+
+####################
+
+read -p "Enter the start range " start
+read -p "Enter the end range " end
+
+echo $start
+echo $end
